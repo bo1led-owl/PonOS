d9d4738c4450b6b6c43f0081ea5e9b1aee96ccc7
Ilya Sovtsov <70143303+bo1led-owl@users.noreply.github.com>
2025-10-07 15:55:26 +0700

    Task 04 (#5)
    * task 4
    
    * formatting
    
    * fix repeating rebuilding & put a value into `ebp`
    
    * review fixes & build script strictification

--- src/assert.h
+++ src/assert.h
@@ -5,5 +5,6 @@
 #ifdef NDEBUG
 #define assert(cond) ((cond) ? (void)(0) : __builtin_unreachable())
 #else
-#define assert(cond) ((cond) ? (void)(0) : panic(__FILE__ ":%d: assertion " #cond " failed", __LINE__))
+#define assert(cond) \
+    ((cond) ? (void)(0) : panic(__FILE__ ":%d: assertion " #cond " failed", __LINE__))
 #endif
--- src/main.c
+++ src/main.c
@@ -1,26 +1,34 @@
-#include "alloc.h"
-#include "utils.h"
-#include "vga.h"
+#include "asmUtils.h"
+#include "interrupts.h"
+
+#define SETUP_REGISTERS \
+    __asm__ volatile(   \
+        "mov eax, 0\n"  \
+        "mov ecx, 1\n"  \
+        "mov edx, 2\n"  \
+        "mov ebx, 3\n"  \
+        "mov ebp, 4\n"  \
+        "mov esi, 5\n"  \
+        "mov edi, 6\n")
+
+#define divisionByZeroExperiment \
+    SETUP_REGISTERS;             \
+    __asm__ volatile("idiv eax\n");
+
+#define syscallExperiment \
+    SETUP_REGISTERS;      \
+    __asm__ volatile("int 42\n");
+
+#define ioExperiment \
+    SETUP_REGISTERS; \
+    __asm__ volatile("sti\n");
 
 [[noreturn]] void kernelEntry() {
-    WindowHandle windows[4];
-    windows[0] = addWindow(0, 0, 10, 40);
-    windows[1] = addWindow(40, 0, 10, 40);
-    windows[2] = addWindow(0, 10, 15, 40);
-    windows[3] = addWindow(40, 10, 15, 40);
-    initScreen();
+    setupInterrupts();
 
-    setFgColor(windows[1], Color_Red);
-    setFgColor(windows[2], Color_Cyan);
-    setFgColor(windows[3], Color_Green);
-
-    for (usize i = 0;; i += 27) {
-        usize size = i % 256;
-        usize alignment = i % 64;
-        printf(windows[i % 4], "size = %u alignment = %u: ", size, alignment);
-        void* p = mallocImmortal(size, alignment);
-        printf(windows[i % 4], "0x%p\n", p);
-    }
+    // divisionByZeroExperiment
+    // syscallExperiment
+    ioExperiment
 
     infiniteLoop();
 }
